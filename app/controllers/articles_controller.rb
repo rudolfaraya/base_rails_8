5dcde23945d3de84ebdcb2fd9fc668241bc2571e
Rodolfo Araya <rudolfaray2@gmail.com>
2024-11-14 17:48:48 -0300

    Agregar formulario articles new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,23 @@ class ArticlesController < ApplicationController
     # params = {id: 1}
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(articles_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def articles_params
+    params.require(:article).permit(:title, :body)
+  end
 end
